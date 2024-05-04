c4b3b1945779ae79fbae2746515f3b6e31f0349a
Michelle Quinn <42622727+shellbellquinn@users.noreply.github.com>
2024-05-04 15:27:45 -0500

    Add files via upload

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,9 +3,16 @@ from . import views
 
 app_name = 'recipe'
 urlpatterns = [
+    path('', views.view_recipe, name='view_recipes'),
+    path('mine/', views.view_my_recipes, name='view_my_recipes'),
+    path('detail/<int:recipe_primary_key>/', views.view_recipe_detail, name='view_recipe_detail'),
     path('create/', views.create_recipe, name="create_recipe"),
     path('update/<int:recipe_primary_key>/', views.update_recipe, name="update_recipe"),
     path('delete/<int:recipe_primary_key>/', views.delete_recipe, name="delete_recipe"),
     path('<int:recipe_primary_key>/ingredient/create/', views.create_ingredient, name="create_ingredient"),
     path('<int:recipe_primary_key>/ingredient/update/<int:ingredient_primary_key>/', views.update_ingredient, name="update_ingredient"),
+    path('<int:recipe_primary_key>/ingredient/delete/<int:ingredient_primary_key>/', views.delete_ingredient, name="delete_ingredient"),
+    path('<int:recipe_primary_key>/instructions/create/', views.create_instructions, name="create_instructions"),
+    path('<int:recipe_primary_key>/instructions/update/<int:instructions_primary_key>/', views.update_instructions, name="update_ingredient"),
+    path('<int:recipe_primary_key>/instructions/delete/<int:instructions_primary_key>/', views.delete_instructions, name="delete_ingredient"),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,11 +1,56 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
 
 from .models import Category, Recipe, Instructions, Ingredient
 from .forms import RecipeForm, IngredientForm, InstructionForm
 
+#view user recipes in view_my_recipes.html
+@login_required
+def view_my_recipes(request):
+    recipes = Recipe.objects.filter(created_by=request.user)
+
+    return render(request, 'recipe/view_my_recipes.html', {
+    'title': 'My Recipes',
+    'recipes': recipes,
+})
+
+#view recipes in view_recipe.html
 @login_required 
+def view_recipe(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories= Category.objects.all()
+    recipes = Recipe.objects.all()
+
+    if category_id:
+        recipes = recipes.filter(category_id=category_id)
+       
+    if query:
+        recipes = recipes.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'recipe/view_recipe.html', {
+        'title': 'Recipes',
+        'recipes': recipes,
+        'categories': categories,
+    })
+
+#view recipe details
+@login_required
+def view_recipe_detail(request, recipe_primary_key):
+    recipe = get_object_or_404(Recipe, pk=recipe_primary_key)
+    ingredients = Ingredient.objects.filter(recipe_id=recipe_primary_key)
+    instructions = Instructions.objects.filter(recipe_id=recipe_primary_key)
+    return render(request, 'recipe/view_recipe_details.html', {
+        'title': 'Recipe Details',
+        'recipe': recipe,
+        'ingredients': ingredients,
+        'instructions': instructions,
+    })
+    
+#create a new recipe
+@login_required
 def create_recipe(request):
     if request.method == 'POST':
         form = RecipeForm(request.POST, request.FILES)
@@ -22,8 +67,10 @@ def create_recipe(request):
     return render(request, 'recipe/forms.html', {
         'title': 'Create Recipe',
         'form': form,
+        'submitmessage': 'Submit and Procceed to Next Step: Add Ingredients'
     })
 
+#update recipe
 @login_required
 def update_recipe(request, recipe_primary_key):
     recipe = get_object_or_404(Recipe, pk=recipe_primary_key, created_by=request.user)
@@ -47,6 +94,7 @@ def update_recipe(request, recipe_primary_key):
         'form': form
     })
 
+#delete recipe
 @login_required
 def delete_recipe(request, recipe_primary_key):
     recipe = get_object_or_404(Recipe, pk=recipe_primary_key, created_by=request.user)
@@ -55,6 +103,7 @@ def delete_recipe(request, recipe_primary_key):
     messages.success(request, 'Recipe deleted')
     return(redirect('core:home'))
 
+#create ingredients associated with recipe
 @login_required
 def create_ingredient(request, recipe_primary_key):
     if request.method == 'POST':
@@ -73,8 +122,10 @@ def create_ingredient(request, recipe_primary_key):
     return render(request, 'recipe/forms.html', {
         'title': 'Add Ingredient',
         'form': form,
+        'submitmessage': 'Submit and Procceed to Next Step: Add Instructions'
     })
 
+#update ingredient
 @login_required
 def update_ingredient(request, recipe_primary_key, ingredient_primary_key):
     recipe = get_object_or_404(Recipe, pk=recipe_primary_key, created_by=request.user)
@@ -96,3 +147,71 @@ def update_ingredient(request, recipe_primary_key, ingredient_primary_key):
         'title': "Update Recipe Ingredient",
         'form': form,
     })
+
+#delete ingredient
+@login_required
+def delete_ingredient(request, recipe_primary_key, ingredient_primary_key):
+    recipe = get_object_or_404(Recipe, pk=recipe_primary_key, created_by=request.user)
+    ingredient = get_object_or_404(Ingredient, pk=ingredient_primary_key, recipe_id=recipe.id)
+    ingredient.delete()
+
+    messages.success(request, 'Ingredient deleted')
+    return(redirect('core:home'))
+
+#create instructions for recipe
+@login_required
+def create_instructions(request, recipe_primary_key):
+    if request.method == 'POST':
+        form = InstructionForm(request.POST)
+
+        if form.is_valid():
+            instructions = form.save(commit=False)
+            instructions.recipe_id = recipe_primary_key
+            instructions.save()
+            
+            messages.success(request, "Instructions Added")
+        else:
+            messages.error(request, "Failed to add instructions")
+    else:
+        form = InstructionForm()
+    return render(request, 'recipe/forms.html', {
+        'title': 'Add Instructions',
+        'form': form,
+        'submitmessage': 'Submit and Save Recipe'
+    })
+
+#edit instructions
+@login_required
+def update_instructions(request, recipe_primary_key, instructions_primary_key):
+    recipe = get_object_or_404(Recipe, pk=recipe_primary_key, created_by=request.user)
+    instructions = get_object_or_404(Instructions, pk=instructions_primary_key, recipe_id=recipe.id)
+
+    if request.method == 'POST':
+        form = InstructionForm(request.POST, instance=instructions)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Instructions updated")
+        else:
+            messages.error(request, "Failed to update instructions")
+    else:
+        form = InstructionForm(instance=instructions)
+    
+    form = InstructionForm(instance=instructions)
+    return render(request, 'recipe/forms.html', {
+        'title': "Update Recipe Instructions",
+        'form': form,
+    })
+    
+
+
+#delete instructions
+@login_required
+def delete_instructions(request, recipe_primary_key, instructions_primary_key):
+    recipe = get_object_or_404(Recipe, pk=recipe_primary_key, created_by=request.user)
+    instructions = get_object_or_404(Instructions, pk=instructions_primary_key, recipe_id=recipe.id)
+    instructions.delete()
+
+    messages.success(request, 'Instructions deleted')
+    return(redirect('core:home'))
+    
